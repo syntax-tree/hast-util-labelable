5e5cc5a172a1c3af947ead0fe704bfd3ef79df30
Titus Wormer <tituswormer@gmail.com>
2021-07-23 09:28:54 +0200

    Remove generated files

--- index.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Check whether a `node` is labelable:
- * See: <https://html.spec.whatwg.org/#category-label>.
- *
- * @param {unknown} node
- * @returns {boolean}
- */
-export function labelable(node: unknown): boolean;
