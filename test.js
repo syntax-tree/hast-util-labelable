e996aa9f7ebf0a0f6f93637070b5df01870f5d04
Titus Wormer <tituswormer@gmail.com>
2023-07-31 18:11:56 +0200

    Refactor code-style

--- test.js
+++ test.js
@@ -1,126 +1,152 @@
 import assert from 'node:assert/strict'
 import test from 'node:test'
 import {labelable} from './index.js'
-import * as mod from './index.js'
 
-test('labelable', () => {
-  assert.deepEqual(
-    Object.keys(mod).sort(),
-    ['labelable'],
-    'should expose the public api'
+test('labelable', async function (t) {
+  await t.test('should expose the public api', async function () {
+    assert.deepEqual(Object.keys(await import('./index.js')).sort(), [
+      'labelable'
+    ])
+  })
+
+  await t.test('should return `false` without node', async function () {
+    // @ts-expect-error: check that the runtime supports a missing node.
+    assert.equal(labelable(), false)
+  })
+
+  await t.test('should return `false` without element', async function () {
+    assert.equal(labelable({type: 'text', value: 'Alpha'}), false)
+  })
+
+  await t.test(
+    'should return `false` for non-labelable elements',
+    async function () {
+      assert.equal(
+        labelable({
+          type: 'element',
+          tagName: 'div',
+          properties: {},
+          children: []
+        }),
+        false
+      )
+    }
   )
 
-  // @ts-expect-error: runtime.
-  assert.equal(labelable(), false, 'should return `false` without node')
+  await t.test('should return `true` for `input` elements', async function () {
+    assert.equal(
+      labelable({
+        type: 'element',
+        tagName: 'input',
+        properties: {},
+        children: []
+      }),
+      true
+    )
+  })
 
-  assert.equal(
-    labelable({type: 'text', value: 'Alpha'}),
-    false,
-    'should return `false` without element'
+  await t.test(
+    'should return `false` for `input[type=hidden]` elements',
+    async function () {
+      assert.equal(
+        labelable({
+          type: 'element',
+          tagName: 'input',
+          properties: {type: 'hidden'},
+          children: []
+        }),
+        false
+      )
+    }
   )
 
-  assert.equal(
-    labelable({type: 'element', tagName: 'div', properties: {}, children: []}),
-    false,
-    'should return `false` for non-labelable elements'
+  await t.test('should return `true` for `button` elements', async function () {
+    assert.equal(
+      labelable({
+        type: 'element',
+        tagName: 'button',
+        properties: {},
+        children: []
+      }),
+      true
+    )
+  })
+
+  await t.test('should return `true` for `keygen` elements', async function () {
+    assert.equal(
+      labelable({
+        type: 'element',
+        tagName: 'keygen',
+        properties: {},
+        children: []
+      }),
+      true
+    )
+  })
+
+  await t.test('should return `true` for `meter` elements', async function () {
+    assert.equal(
+      labelable({
+        type: 'element',
+        tagName: 'meter',
+        properties: {},
+        children: []
+      }),
+      true
+    )
+  })
+
+  await t.test('should return `true` for `output` elements', async function () {
+    assert.equal(
+      labelable({
+        type: 'element',
+        tagName: 'output',
+        properties: {},
+        children: []
+      }),
+      true
+    )
+  })
+
+  await t.test(
+    'should return `true` for `progress` elements',
+    async function () {
+      assert.equal(
+        labelable({
+          type: 'element',
+          tagName: 'progress',
+          properties: {},
+          children: []
+        }),
+        true
+      )
+    }
   )
 
-  assert.equal(
-    labelable({
-      type: 'element',
-      tagName: 'input',
-      properties: {},
-      children: []
-    }),
-    true,
-    'should return `true` for `input` elements'
-  )
+  await t.test('should return `true` for `select` elements', async function () {
+    assert.equal(
+      labelable({
+        type: 'element',
+        tagName: 'select',
+        properties: {},
+        children: []
+      }),
+      true
+    )
+  })
 
-  assert.equal(
-    labelable({
-      type: 'element',
-      tagName: 'input',
-      properties: {type: 'hidden'},
-      children: []
-    }),
-    false,
-    'should return `false` for `input[type=hidden]` elements'
-  )
-
-  assert.equal(
-    labelable({
-      type: 'element',
-      tagName: 'button',
-      properties: {},
-      children: []
-    }),
-    true,
-    'should return `true` for `button` elements'
-  )
-
-  assert.equal(
-    labelable({
-      type: 'element',
-      tagName: 'keygen',
-      properties: {},
-      children: []
-    }),
-    true,
-    'should return `true` for `keygen` elements'
-  )
-
-  assert.equal(
-    labelable({
-      type: 'element',
-      tagName: 'meter',
-      properties: {},
-      children: []
-    }),
-    true,
-    'should return `true` for `meter` elements'
-  )
-
-  assert.equal(
-    labelable({
-      type: 'element',
-      tagName: 'output',
-      properties: {},
-      children: []
-    }),
-    true,
-    'should return `true` for `output` elements'
-  )
-
-  assert.equal(
-    labelable({
-      type: 'element',
-      tagName: 'progress',
-      properties: {},
-      children: []
-    }),
-    true,
-    'should return `true` for `progress` elements'
-  )
-
-  assert.equal(
-    labelable({
-      type: 'element',
-      tagName: 'select',
-      properties: {},
-      children: []
-    }),
-    true,
-    'should return `true` for `select` elements'
-  )
-
-  assert.equal(
-    labelable({
-      type: 'element',
-      tagName: 'textarea',
-      properties: {},
-      children: []
-    }),
-    true,
-    'should return `true` for `textarea` elements'
+  await t.test(
+    'should return `true` for `textarea` elements',
+    async function () {
+      assert.equal(
+        labelable({
+          type: 'element',
+          tagName: 'textarea',
+          properties: {},
+          children: []
+        }),
+        true
+      )
+    }
   )
 })
